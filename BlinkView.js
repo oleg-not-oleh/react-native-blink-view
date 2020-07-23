401d7322e2641f835eb55e75b642b0c382f56d53
Dima <dimakorniets@gmail.com>
2020-07-24 00:30:32 +0300

    Update BlinkView.js

--- BlinkView.js
+++ BlinkView.js
@@ -1,20 +1,8 @@
-/* @flow */
-/* eslint-disable prefer-const */
-/* jshint esversion: 6 */
-/* eslint-env node, mocha */
-'use strict';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {View, Animated} from 'react-native';
 
-import React, { Component } from "react";
-import PropTypes            from "prop-types";
-import { View, Animated }   from "react-native";
-
-export default class BlinkView extends Component
-{
-  /**
-   * Local timer handled function. Destroyed on unmount
-   * @type {Function}
-   */
-  _onDelay:any = null;
+export default class BlinkView extends Component {
 
   /**
    * Props validation
@@ -25,103 +13,119 @@ export default class BlinkView extends Component
    *   blinking : {boolean} Defines if the element is currently blinkink, Default is true
    * }
    */
-  static propTypes:Object = {
-    element  : PropTypes.any,
-    children : PropTypes.any,
-    delay    : PropTypes.number,
-    blinking : PropTypes.bool
-  }
+  static propTypes: Object = {
+    element: PropTypes.any,
+    children: PropTypes.any,
+    delay: PropTypes.number,
+    blinking: PropTypes.bool,
+  };
 
   /**
    * Default props
    * @return {object} Default props for this element.
    */
-  static defaultProps:Object = {
-    element  : View,
-    children : null,
-    delay    : 1500,
-    blinking : true
-  }
+  static defaultProps: Object = {
+    element: View,
+    children: null,
+    delay: 1500,
+    blinking: true,
+  };
 
   /**
    * Init states
    * @return {object} states
    */
-  constructor( props:Object, state:Object ):Object
-  {
-    try
-    {
-      super( props );
+  constructor(props: Object, state: Object): Object {
+    try {
+      super(props);
 
       this.state = {
-        delay     : props && props.delay || 1500,
-        blinkAnim : new Animated.Value( 0 )
+        delay: (props && props.delay) || 1500,
+        blinkAnim: new Animated.Value(0),
       };
-    }
-    catch ( err )
-    {
-      console.warn( err );
+    } catch (err) {
+      console.warn(err);
     }
   }
 
-  componentDidMount():void
-  {
-    try
-    {
-      if ( this.props.blinking === true )
-      {
-        clearInterval( this._onDelay );
-        this._onDelay = setInterval( ():void =>
-        {
-          this.state.blinkAnim.stopAnimation();
-          Animated.timing( this.state.blinkAnim, {
-            toValue   : this.state.blinkAnim._value === 0 ? 1 : 0,
-            duration  : this.state.delay-1,
-            useNativeDriver: false
-          }).start();
-        }, this.state.delay+1 );
-      }
-    }
-    catch ( err )
-    {
-      console.warn( err );
-    }
-  }
+  startAnim = () => {
+    this.state.blinkAnim.stopAnimation(() => {
+      this.setState({blinkAnim: new Animated.Value(0)}, () => {
+        Animated.loop(
+          Animated.sequence([
+            Animated.timing(this.state.blinkAnim, {
+              toValue: 0,
+              duration: this.state.delay,
+              delay: this.state.delay / 3,
+              useNativeDriver: true,
+            }),
+            Animated.timing(this.state.blinkAnim, {
+              toValue: 1,
+              duration: this.state.delay,
+              useNativeDriver: true,
+            }),
+          ]),
+        ).start();
+      });
+    });
+  };
 
-  componentWillUnmount():void
-  {
-    try
-    {
-      clearInterval( this._onDelay );
+  componentDidMount = () => {
+    if (this.props.blinking === true) {
+      this.startAnim();
     }
-    catch ( err )
-    {
-      console.warn( err );
+  };
+
+  componentDidUpdate = (
+    prevProps: Readonly<P>,
+    prevState: Readonly<S>,
+    snapshot: SS,
+  ) => {
+    if (prevProps.blinking != this.props.blinking) {
+      this.props.blinking
+        ? this.startAnim()
+        : this.state.blinkAnim.stopAnimation(() => {
+            this.setState({blinkAnim: new Animated.Value(1)});
+          });
     }
-  }
+    if(prevProps.delay != this.props.delay){
+        this.setState({delay: this.props.delay}, ()=>{
+          this.props.blinking
+              ? this.startAnim()
+              : this.state.blinkAnim.stopAnimation(() => {
+                this.setState({blinkAnim: new Animated.Value(1)});
+              });
+        })
+    }
+  };
 
-  render():any
-  {
-    try
-    {
-      const isBlinking:bolean = this.props && this.props.blinking || true;
-      const Element:any = ( ( isBlinking === true ) ? Animated.createAnimatedComponent( this.props && this.props.element || View ) : this.props && this.props.element || View );
+  componentWillUnmount = () => {
+    this.state.blinkAnim.stopAnimation();
+  };
 
-      return  (
+  render() {
+    try {
+      const isBlinking: bolean = (this.props && this.props.blinking) || true;
+      const Element: any =
+        isBlinking === true
+          ? Animated.createAnimatedComponent(
+              (this.props && this.props.element) || View,
+            )
+          : (this.props && this.props.element) || View;
+
+      return (
         <Element
           {...this.props}
-          style = {[this.props.style, { opacity: ( isBlinking === true ) ? this.state.blinkAnim : 1 } ]}>
-          {this.props && this.props.children || null}
+          style={[
+            this.props.style,
+            {opacity: isBlinking === true ? this.state.blinkAnim : 1},
+          ]}>
+          {(this.props && this.props.children) || null}
         </Element>
-      )
+      );
+    } catch (err) {
+      console.warn(err);
     }
-    catch ( err )
-    {
-      console.warn( err );
-    }
-    return (
-      this.props && this.props.children || <View />
-    );
+    return (this.props && this.props.children) || <View />;
   }
-
 }
